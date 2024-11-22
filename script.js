18bb9c6b82856829db089e9e8886d785523c6d26
Rolo <137957440+Rolovg@users.noreply.github.com>
2024-11-22 22:09:23 +0100

    Update script.js

--- script.js
+++ script.js
@@ -12,15 +12,14 @@ const pegRadius = 5;
 const gravity = 0.1;
 const bounceFactor = 0.7;
 
-let score = 100; 
+let score = 100;
 let ballCost = 1; 
-const slotMultipliers = [1, 2, 3, 4, 5, 5, 4, 3, 2, 1];
-
+const slotMultipliers = [1, 2, 3, 4, 5, 5, 4, 3, 2, 1]; 
 
 function drawScore() {
-    ctx.fillStyle = 'black';
-    ctx.font = '16px Arial';
-    ctx.fillText(`Score: ${score}`, 10, 20);
+    ctx.fillStyle = 'lime';
+    ctx.font = '20px Arial';
+    ctx.fillText(`Score: ${score}`, 10, 25);
 }
 
 
@@ -43,7 +42,7 @@ function setupPegs() {
 
 
 function drawPegs() {
-    ctx.fillStyle = 'black';
+    ctx.fillStyle = 'aqua';
     pegs.forEach(peg => {
         ctx.beginPath();
         ctx.arc(peg.x, peg.y, pegRadius, 0, Math.PI * 2);
@@ -62,10 +61,10 @@ function setupSlots() {
 
 function drawSlots() {
     slots.forEach(slot => {
-        ctx.fillStyle = 'gray';
+        ctx.fillStyle = 'purple';
         ctx.fillRect(slot.x, canvas.height - 30, slot.width, 30);
-        ctx.fillStyle = 'black';
-        ctx.font = '12px Arial';
+        ctx.fillStyle = 'white';
+        ctx.font = '14px Arial';
         ctx.fillText(`x${slot.multiplier}`, slot.x + slot.width / 2 - 10, canvas.height - 10);
     });
 }
@@ -74,12 +73,15 @@ function drawBall(ball) {
     ctx.fillStyle = 'red';
     ctx.beginPath();
     ctx.arc(ball.x, ball.y, ballRadius, 0, Math.PI * 2);
+    ctx.strokeStyle = 'yellow'; 
+    ctx.lineWidth = 2;
+    ctx.stroke();
     ctx.fill();
 }
 
 
-function updateBall(ball) {
-    ball.vy += gravity;
+function updateBall(ball, index) {
+    ball.vy += gravity; 
     ball.x += ball.vx;
     ball.y += ball.vy;
 
@@ -95,19 +97,19 @@ function updateBall(ball) {
         }
     });
 
-
+  
     if (ball.x < ballRadius || ball.x > canvas.width - ballRadius) {
         ball.vx *= -1;
     }
 
-
+    
     if (ball.y + ballRadius >= canvas.height - 30) {
-      
         const slot = slots.find(slot => ball.x >= slot.x && ball.x < slot.x + slot.width);
-        if (slot) {
+        if (slot && !ball.scored) {
             score += slot.multiplier; 
+            ball.scored = true; 
         }
-        ball.remove = true; 
+        balls.splice(index, 1); 
     }
 }
 
@@ -119,7 +121,8 @@ function dropBall() {
             x: canvas.width / 2,
             y: ballRadius,
             vx: Math.random() * 2 - 1, 
-            vy: 0
+            vy: 0,
+            scored: false
         };
         balls.push(ball);
     }
@@ -131,14 +134,8 @@ function update() {
     drawSlots();
     drawScore();
 
-    balls.forEach(ball => updateBall(ball));
+    balls.forEach((ball, index) => updateBall(ball, index));
     balls.forEach(ball => drawBall(ball));
-
-    for (let i = balls.length - 1; i >= 0; i--) {
-        if (balls[i].remove) {
-            balls.splice(i, 1);
-        }
-    }
 
     requestAnimationFrame(update);
 }
@@ -146,4 +143,3 @@ function update() {
 setupPegs();
 setupSlots();
 update();
-
